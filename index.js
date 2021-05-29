faf4415c7378eaa61237a55d2c71d87bfa0665ed
tawasu <raigo12@yahoo.co.jp>
2021-05-29 18:49:08 +0900

    2 files modified

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 // Import stylesheets
-import "./style.css";
+import './style.css';
 // Firebase App (the core Firebase SDK) is always required and must be listed first
-import firebase from "firebase/app";
+import firebase from 'firebase/app';
 
 // Add the Firebase products that you want to use
-import "firebase/auth";
-import "firebase/firestore";
+import 'firebase/auth';
+import 'firebase/firestore';
 
-import * as firebaseui from "firebaseui";
+import * as firebaseui from 'firebaseui';
 
 // Document elements
-const startRsvpButton = document.getElementById("startRsvp");
-const guestbookContainer = document.getElementById("guestbook-container");
+const startRsvpButton = document.getElementById('startRsvp');
+const guestbookContainer = document.getElementById('guestbook-container');
 
-const form = document.getElementById("leave-message");
-const input = document.getElementById("message");
-const guestbook = document.getElementById("guestbook");
-const numberAttending = document.getElementById("number-attending");
-const rsvpYes = document.getElementById("rsvp-yes");
-const rsvpNo = document.getElementById("rsvp-no");
+const form = document.getElementById('leave-message');
+const input = document.getElementById('message');
+const guestbook = document.getElementById('guestbook');
+const numberAttending = document.getElementById('number-attending');
+const rsvpYes = document.getElementById('rsvp-yes');
+const rsvpNo = document.getElementById('rsvp-no');
 
 var rsvpListener = null;
 var guestbookListener = null;
 
 async function main() {
   // Add Firebase project configuration object here
+  var firebaseConfig = {
+    apiKey: 'AIzaSyAJGJEVzx5QLblVU-_dgxbYjFf-mjbv8lM',
+    authDomain: 'fir-web-codelab-b3130.firebaseapp.com',
+    projectId: 'fir-web-codelab-b3130',
+    storageBucket: 'fir-web-codelab-b3130.appspot.com',
+    messagingSenderId: '459321090316',
+    appId: '1:459321090316:web:f563e7891fc0c8759c2929'
+  };
   // var firebaseConfig = {};
 
   // firebase.initializeApp(firebaseConfig);
@@ -44,7 +52,11 @@ async function main() {
       }
     }
   };
-
-  // const ui = new firebaseui.auth.AuthUI(firebase.auth());
+  // Initialize the FirebaseUI widget using Firebase
+  const ui = new firebaseui.auth.AuthUI(firebase.auth());
+  // Listen to RSVP button clicks
+  startRsvpButton.addEventListener('click', () => {
+    ui.start('#firebaseui-auth-container', uiConfig);
+  });
 }
 main();
